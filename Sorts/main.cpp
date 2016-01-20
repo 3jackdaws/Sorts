f2096e12abc87157a8ee8bbbebb06772fff62ae4
Ian Murphy <immur.50ae@gmail.com>
2016-01-20 13:06:41 -0800

    initial commit

---
+++ Sorts/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorts
+//
+//  Created by Ian Murphy on 1/20/16.
+//  Copyright © 2016 Ian Murphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
